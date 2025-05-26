f032374d69791e66c644067413c0f89be2f75cc3
Abdullahi-Ai <abdullahiibrashair@gmail.com>
2025-05-26 11:40:40 +0300

    created a class property def id which defines the getter for the id attribute and the other property is define the name attributes

--- code-challenge/lib/models/article.py
+++ code-challenge/lib/models/article.py
@@ -4,3 +4,11 @@ class Author:
     def __init__(self, id=None, name=None):
         self._id = id
         self.name = name
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def name(self):
+        return self._name
