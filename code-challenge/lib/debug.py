fb8f68353a1d3adc6e7fc123bb2cf06ca9e57153
Abdullahi-Ai <abdullahiibrashair@gmail.com>
2025-05-26 12:04:23 +0300

    created a def setup db in order to create three tables search as author, magazine and artcle this line in def setup it accept the database cursor object which is used to excute sql commands

--- code-challenge/lib/debug.py
+++ code-challenge/lib/debug.py
@@ -6,3 +6,27 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 import sqlite3
 from lib.models.author import Author
+
+def setup_db(cursor):
+    cursor.execute("""
+    CREATE TABLE authors (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL
+    )
+    """)
+    cursor.execute("""
+    CREATE TABLE magazines (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL
+    )
+    """)
+    cursor.execute("""
+    CREATE TABLE articles (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        author_id INTEGER NOT NULL,
+        magazine_id INTEGER NOT NULL,
+        FOREIGN KEY(author_id) REFERENCES authors(id),
+        FOREIGN KEY(magazine_id) REFERENCES magazines(id)
+    )
+    """)
