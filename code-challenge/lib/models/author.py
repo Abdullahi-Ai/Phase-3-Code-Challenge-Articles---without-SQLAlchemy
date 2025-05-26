01e29f978718ef6f8f0eef499e3f3c55ea19d82b
Abdullahi-Ai <abdullahiibrashair@gmail.com>
2025-05-26 09:59:38 +0300

    created methods property which can be used to define the methods that can be used as a attributes

--- code-challenge/lib/models/author.py
+++ code-challenge/lib/models/author.py
@@ -2,3 +2,16 @@ class Author:
     def __init__(self, id=None, name=None):
         self._id = id
         self.name = name 
+
+        @property
+        def id(self):
+            return self._id
+        
+        @property
+        def name(self):
+            return self._name
+           
+        
+
+
+
